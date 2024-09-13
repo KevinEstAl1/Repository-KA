c9eaf0305aa8301acf8eb547557639528cc9024a
KevinEstAl1 <kevin.aguilar.est@tecazuay.edu.ec>
2024-09-12 21:12:09 -0500

    Delete LAB 2 directory

--- LAB 2/__init__.py
+++
@@ -1,5 +0,0 @@
-from clase import estudiante_registrado_en_materia
-
-print(estudiante_registrado_en_materia("Daniel", "Matematica"))  # True
-print(estudiante_registrado_en_materia("Daniel", "Biologia"))    # False
-print(estudiante_registrado_en_materia("Maria", "Fisica"))       # True
